a11aeea83bfa8abb543e49e166cdb55e1a0d0071
Dahye Kim <digeuthi@gmail.com>
2023-07-13 16:14:57 +0900

    page modify

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,12 +1,11 @@
 <template>
-    <div> </div>
     <header>
+      <div></div>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/login">로그인</router-link> |
       <router-link to="/board/list">게시판</router-link> 
-      
     </div>
   </header>
   <hr/>
