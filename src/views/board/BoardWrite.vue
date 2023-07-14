f52d01871c572beed5499251c32e6f35e2844999
Dahye Kim <digeuthi@gmail.com>
2023-07-14 14:58:44 +0900

    editor basic make

--- src/views/board/BoardWrite.vue
+++ src/views/board/BoardWrite.vue
@@ -8,6 +8,11 @@
         <input type="text" v-model="title" class="w3-input w3-border" placeholder="제목을 입력해주세요.">
         <input type="text" v-model="author" class="w3-input w3-border" placeholder="작성자를 입력해주세요." v-if="idx === undefined">
       </div>
+      <div class="editor-wrapper">
+        <textarea v-model="content" class="editor"></textarea>
+        <input type="file" @change="handleFileUpload" />
+        <img v-if="uploadedImage" :src="uploadedImage" alt="Uploaded Image" />
+      </div>
       <div class="board-contents">
         <textarea id="" cols="30" rows="10" v-model="contents" class="w3-input w3-border" style="resize: none;">
         </textarea>
@@ -101,5 +106,15 @@
   }
   </script>
   <style scoped>
-  
+    .editor-container {
+      background-color: #f2f2f2;
+      border: 1px solid #ccc;
+      padding: 10px;
+    }
+
+    .ck-editor__editable {
+      min-height: 200px;
+      padding: 10px;
+      border: none;
+    }
   </style>
