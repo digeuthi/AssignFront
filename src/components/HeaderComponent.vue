bca909ea98b81c205f7e41eea44b488df52ce9ed
Dahye Kim <digeuthi@gmail.com>
2023-07-14 14:30:52 +0900

    home about hide

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -2,8 +2,8 @@
     <header>
       <div></div>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> | -->
       <router-link to="/login">로그인</router-link> |
       <router-link to="/board/list">게시판</router-link> 
     </div>
